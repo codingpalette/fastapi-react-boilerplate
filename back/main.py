9ae7ace0d012034be1bd6134a4fd034574a1bb18
codingpalette <codingpalette@gmail.com>
2021-11-04 15:30:29 +0900

    main.py 수정

--- back/main.py
+++ back/main.py
@@ -1,43 +1,15 @@
 from typing import Optional
 
-import uvicorn
 from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
-from fastapi.middleware.cors import CORSMiddleware
-from middlewares.token_validator import access_control
-from routes import user
+
+app = FastAPI()
 
 
-def create_app():
-    app = FastAPI()
-
-    origins = [
-        "http://localhost:3000",
-    ]
-
-    # 미들웨어 정의
-    app.add_middleware(middleware_class=BaseHTTPMiddleware, dispatch=access_control)
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-    @app.get("/")
-    def read_root():
-        return {"Hello": "World"}
-
-
-    @app.get("/items/{item_id}")
-    def read_item(item_id: int, q: Optional[str] = None):
-        return {"item_id": item_id, "q": q}
-
-    app.include_router(user.router, tags=["user"], prefix="/api")
-    return app
-
-
-app = create_app()
-
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
